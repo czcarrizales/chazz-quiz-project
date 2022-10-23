ddce9637c991bd3fb2995b215b71de6ce2648710
Chazz <czcarrizales@yahoo.com>
2022-10-23 15:55:54 -0700

    edit quizmodel to include a question and relate an array of answers to allow answer models to be pushed into the answers array

--- backend/models/quizModel.js
+++ backend/models/quizModel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
+const {Schema} = mongoose;
+const Answer = require('./answerModel')
 
-const quizSchema = new mongoose.Schema({
-    title: String
+const quizSchema = new Schema({
+    title: String,
+    question: String,
+    answers: [{type: Schema.Types.ObjectId, ref: 'Answer'}]
 })
 
 const Quiz = mongoose.model('Quiz', quizSchema)
