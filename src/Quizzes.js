1691462e76ee8461a67d0d12d9589967e413d3d7
czcarrizales <czcarrizales@yahoo.com>
2022-10-23 18:30:19 -0700

    remove unnecessary setState

--- src/Quizzes.js
+++ src/Quizzes.js
@@ -1,4 +1,4 @@
-import React, {setState, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
