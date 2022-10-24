19601bfdde56c288e2ccb4962f4e7d1559c33f1d
czcarrizales <czcarrizales@yahoo.com>
2022-10-24 13:19:27 -0700

    refactor ViewQuiz component to retrieve data correctly

--- src/ViewQuiz.js
+++ src/ViewQuiz.js
@@ -1,28 +1,26 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function ViewQuiz() {
 
     const params = useParams()
     const [quiz, setQuiz] = useState(null)
+    const [questions, setQuestions] = useState(null)
     const [answers, setAnswers] = useState(null)
     const [isLoading, setLoading] = useState(true)
 
     useEffect(() => {
         getOneQuiz()
-        console.log('component loaded')
+        console.log('component loadeds')
+        console.log(quiz)
     }, [])
 
     const getOneQuiz = () => {
         axios.get(`http://localhost:5000/quizzes/${params._id}`)
         .then(res => {
             setQuiz(res)
-            setAnswers([res.data.answers])
             setLoading(false)
-            if(res.data.title) {console.log(res.data.title)}
-            if(answers) {console.log(answers)}
-            if(res.data.answers) {console.log(res.data.answers)}
         })
         .catch(err => {
             console.log(err)
@@ -43,15 +41,27 @@ export default function ViewQuiz() {
 
     return (
         <div>
-            <h1>{quiz && quiz.data.title}</h1>
-            {quiz && quiz.data.question != null ? 
-            <div>
-                <h2>{quiz && quiz.data.question}</h2>
-                <ul>
-                {answers && answers[0].map(answer => {return <li><button onClick={() => checkAnswer(answer.correct)}>{answers && answer.answer}</button></li>})}
-                </ul>
-            </div> : 
-            <div>No questions here!</div>}
+            {quiz.data.questions.map(q => {
+                return (
+                    <div>
+                    <h2>{q.question}</h2>
+                    {q.answers.map(a => {
+                        return <li><button onClick={() => checkAnswer(a.correct)}>{a.answer}</button></li>
+                    })}
+                    </div>
+                )
+            })}
         </div>
+        // <div>
+        //     <h1>{quiz && quiz.data.title}</h1>
+        //     {quiz && quiz.data.question != null ? 
+        //     <div>
+        //         <h2>{quiz && quiz.data.question}</h2>
+        //         <ul>
+        //         {answers && answers[0].map(answer => {return <li><button onClick={() => checkAnswer(answer.correct)}>{answers && answer.answer}</button></li>})}
+        //         </ul>
+        //     </div> : 
+        //     <div>No questions here!</div>}
+        // </div>
     )
 }
