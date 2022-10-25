301cb64894d99e8f8c64935c3af07206f9687d3d
Chazz <czcarrizales@yahoo.com>
2022-10-25 12:36:05 -0700

    finish ability to add a question from the front end create quiz page, and have a work in progress for adding answers

--- src/CreateQuiz.js
+++ src/CreateQuiz.js
@@ -1,73 +1,122 @@
-import React, {useEffect, useState} from "react";
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import CreateQuestion from "./CreateQuestion";
+import CreateAnswer from "./CreateAnswer";
 
 export default function CreateQuiz(props) {
+  const [input, setInput] = useState({
+    title: ""
+  });
 
-    const [input, setInput] = useState({
-        title: '',
-        question: ''
-    })
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([])
 
-    const [numOfQuestions, setNumOfQuestions] = useState(0)
+  const [numOfQuestions, setNumOfQuestions] = useState(0);
+  const [numOfAnswers, setNumOfAnswers] = useState([0])
 
-    useEffect(() => {
+  const [questionInputs, setQuestionInputs] = useState([]);
+  const [answerInputs, setAnswerInputs] = useState([])
 
-    }, [numOfQuestions])
-
-    function handleChange(event) {
-        const {name, value} = event.target;
-        console.log(input)
-        setInput(prevInput => {
-            return {
-                ...prevInput, [name]: value
-            }
-        })
+  useEffect(() => {
+    if (answers.length) {
+        console.log('answers updated', answers)
     }
+  }, [answers])
 
-    function createQuiz(event) {
-        event.preventDefault()
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setInput((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      };
+    });
+  }
 
-        const newQuiz = {
-            title: input.title
+  function handleQuestionInputChange(event) {
+    console.log(event)
+    const {id, value} = event.target;
+    const newQuestionInputs = [...questionInputs]
+    newQuestionInputs[id] = value
+    console.log(id, 'id')
+    console.log(value)
+
+    setQuestionInputs(newQuestionInputs);
+
+    console.log(questionInputs, 'questioninputs')
+  }
+
+  function createQuiz(event) {
+    event.preventDefault();
+
+    const newQuiz = {
+      title: input.title,
+    };
+
+    axios
+      .post("http://localhost:5000/quizzes/create", newQuiz)
+      .then((res) => {
+        console.log(res);
+        for (let i = 0; i < numOfQuestions; i++) {
+          const newQuestion = {
+            quiz: res.data._id,
+            question: questionInputs[i],
+          };
+          axios.post(
+            "http://localhost:5000/quizzes/createquestion",
+            newQuestion
+          );
         }
+      })
+      .then((res) => console.log("quiz created", res))
+      .catch((err) => console.log("Error with quiz creation", err));
+  }
 
-        
+  function addQuestion(event) {
+    event.preventDefault();
+    setNumOfQuestions(numOfQuestions + 1);
+    setQuestionInputs(q => [...q, ''])
+    setQuestions((q) => [
+      ...q,
+      <CreateQuestion key={numOfQuestions} id={numOfQuestions}
+        onChange={handleQuestionInputChange}
+        value={questionInputs[numOfQuestions]}
+      />,
+    ]);
+    const newAnswers = [...answers]
+    newAnswers[numOfQuestions] = [<CreateAnswer key={numOfQuestions + 'a'} id={'a' + answers.length} onClick={addAnswer} />]
+    setAnswers(newAnswers)
+  }
 
-        axios.post('http://localhost:5000/quizzes/create', newQuiz)
-            .then(res => {
-                console.log(res)
-                const newQuestion = {
-                    quiz: res.data._id,
-            question: 'just testing this question'
-                }
-                axios.post('http://localhost:5000/quizzes/createquestion', newQuestion)
-            })
-            .then(res => console.log('quiz created', res))
-            .catch(err => console.log('Error with quiz creation', err))
-    }
+  function addAnswer(event) {
+    event.preventDefault()
+    const newAnswers = [...answers]
+    console.log(event.target.id, 'id')
+    console.log(answers.length)
+  }
 
-    function addQuestion(event) {
-        event.preventDefault()
-        setNumOfQuestions(numOfQuestions + 1)
-        console.log(numOfQuestions)
-    }
-
-
-    return (
-        <div>
-            <form>
-                <label for="title">Quiz Title</label>
-                <input onChange={handleChange} name="title" type='text' value={input.title} />
-                <br />
-                <br />
-                { Array(numOfQuestions).fill(<CreateQuestion onChange={handleChange} value={input.question} />) }
-            <button name="question" onClick={addQuestion}>Add Question</button>
-            <br />
-            <br />
-            <button onClick={createQuiz}>Create Quiz</button>
-            </form>
-            
-        </div>
-    )
+  return (
+    <div>
+      <form>
+        <label for="title">Quiz Title</label>
+        <input
+          onChange={handleChange}
+          name="title"
+          type="text"
+          value={input.title}
+        />
+        <br />
+        <br />
+        {questions.map((q) => {
+            return [q, answers[q.props.id].map(a => a)]
+        })}
+        <button name="question" onClick={addQuestion}>
+          Add Question
+        </button>
+        <br />
+        <br />
+        <button onClick={createQuiz}>Create Quiz</button>
+      </form>
+    </div>
+  );
 }
