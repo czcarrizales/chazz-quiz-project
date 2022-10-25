75eb3d37fc63febd7cfbd6ff2ddfcb01c38f570d
Chazz <czcarrizales@yahoo.com>
2022-10-25 12:34:26 -0700

    simplify create question component by only letting it handle creating a question

--- src/CreateQuestion.js
+++ src/CreateQuestion.js
@@ -3,24 +3,9 @@ import CreateAnswer from "./CreateAnswer";
 
 export default function CreateQuestion(props) {
 
-    const [propsinput, setPropsInput] = useState({
-        title: '',
-        question: ''
-    })
-
-    function handleChange(event) {
-        const {name, value} = event.target;
-        setPropsInput(prevInput => {
-            return {
-                ...prevInput, [name]: value
-            }
-        })
-    }
-
     return (
         <div>
-            <input onChange={handleChange} placeholder="Insert question here" name="question" type="text" value={props.value} />
-            <CreateAnswer />
+            <input onChange={props.onChange} id={props.id} placeholder="Insert question here" name="question" type="text" value={props.value} />
         </div>
     )
 }
