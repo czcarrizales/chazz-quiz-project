c7610e830849682500aa81c61bc262e1f9fa6255
Chazz <czcarrizales@yahoo.com>
2022-10-25 12:33:58 -0700

    add attributes and button to CreateAnswer component

--- src/CreateAnswer.js
+++ src/CreateAnswer.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function CreateAnswer() {
+export default function CreateAnswer(props) {
     return (
         <div>
-        <input placeholder="Add Answer Here" />
+        <input onClick={props.onClick} onChange={props.onChange} id={props.id} value={props.value} name="answer" type="text" placeholder="Add Answer Here" />
+        <button onClick={props.onClick}>Add Answer</button>
         </div>
     )
 }
