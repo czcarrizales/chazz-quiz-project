f4addc93969e6876e7d7dd8876ab3f510910c231
Chazz <czcarrizales@yahoo.com>
2022-10-25 23:09:06 -0700

    refactoring work in progress for creating a quiz

--- src/CreateQuiz.js
+++ src/CreateQuiz.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CreateTitle from "./CreateTitle";
 import CreateQuestion from "./CreateQuestion";
 import CreateAnswer from "./CreateAnswer";
 
 export default function CreateQuiz(props) {
   const [input, setInput] = useState({
-    title: ""
+    title: "",
+    question: ""
   });
 
-  const [questions, setQuestions] = useState([]);
-  const [answers, setAnswers] = useState([])
+  // const [testInput, setTestInput] = useState({
+  //   title: "",
+  //   questions: [],
+  //   answers: []
+  // })
+
+  // const [questions, setQuestions] = useState([]);
+  // const [answers, setAnswers] = useState([])
 
   const [numOfQuestions, setNumOfQuestions] = useState(0);
   const [numOfAnswers, setNumOfAnswers] = useState([0])
@@ -17,11 +25,12 @@ export default function CreateQuiz(props) {
   const [questionInputs, setQuestionInputs] = useState([]);
   const [answerInputs, setAnswerInputs] = useState([])
 
-  useEffect(() => {
-    if (answers.length) {
-        console.log('answers updated', answers)
-    }
-  }, [answers])
+  // useEffect(() => {
+  //   if (answers.length) {
+  //       console.log('answers updated', answers)
+  //   }
+  //   console.log(testInput)
+  // }, [answers, testInput])
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -33,18 +42,18 @@ export default function CreateQuiz(props) {
     });
   }
 
-  function handleQuestionInputChange(event) {
-    console.log(event)
-    const {id, value} = event.target;
-    const newQuestionInputs = [...questionInputs]
-    newQuestionInputs[id] = value
-    console.log(id, 'id')
-    console.log(value)
+  // function handleQuestionInputChange(event) {
+  //   console.log(event)
+  //   const {id, value} = event.target;
+  //   const newQuestionInputs = [...questionInputs]
+  //   newQuestionInputs[id] = value
+  //   console.log(id, 'id')
+  //   console.log(value)
 
-    setQuestionInputs(newQuestionInputs);
+  //   setQuestionInputs(newQuestionInputs);
 
-    console.log(questionInputs, 'questioninputs')
-  }
+  //   console.log(questionInputs, 'questioninputs')
+  // }
 
   function createQuiz(event) {
     event.preventDefault();
@@ -60,7 +69,7 @@ export default function CreateQuiz(props) {
         for (let i = 0; i < numOfQuestions; i++) {
           const newQuestion = {
             quiz: res.data._id,
-            question: questionInputs[i],
+            question: input.question,
           };
           axios.post(
             "http://localhost:5000/quizzes/createquestion",
@@ -72,51 +81,58 @@ export default function CreateQuiz(props) {
       .catch((err) => console.log("Error with quiz creation", err));
   }
 
-  function addQuestion(event) {
-    event.preventDefault();
-    setNumOfQuestions(numOfQuestions + 1);
-    setQuestionInputs(q => [...q, ''])
-    setQuestions((q) => [
-      ...q,
-      <CreateQuestion key={numOfQuestions} id={numOfQuestions}
-        onChange={handleQuestionInputChange}
-        value={questionInputs[numOfQuestions]}
-      />,
-    ]);
-    const newAnswers = [...answers]
-    newAnswers[numOfQuestions] = [<CreateAnswer key={numOfQuestions + 'a'} id={'a' + answers.length} onClick={addAnswer} />]
-    setAnswers(newAnswers)
-  }
+  // function addQuestion(event) {
+  //   event.preventDefault();
+  //   setNumOfQuestions(numOfQuestions + 1);
+  //   setQuestionInputs(q => [...q, ''])
+  //   setQuestions((q) => [
+  //     ...q,
+  //     <CreateQuestion key={numOfQuestions} id={numOfQuestions}
+  //       onChange={handleQuestionInputChange}
+  //       value={questionInputs[numOfQuestions]} onClick={addAnswer}
+  //     />,
+  //   ]);
+  //   setTestInput(t => ({...t, questions: [...t.questions, 'hello']}))
+  //   const newAnswers = [...answers]
+  //   newAnswers[numOfQuestions] = [<CreateAnswer key={numOfQuestions + 'a'} id={'a' + answers.length} onClick={addAnswer} />]
+  //   setAnswers(newAnswers)
+  // }
 
-  function addAnswer(event) {
-    event.preventDefault()
-    const newAnswers = [...answers]
-    console.log(event.target.id, 'id')
-    console.log(answers.length)
-  }
+  // function addAnswer(event) {
+  //   event.preventDefault()
+  //   console.log(event)
+  // }
 
   return (
-    <div>
-      <form>
-        <label for="title">Quiz Title</label>
-        <input
-          onChange={handleChange}
-          name="title"
-          type="text"
-          value={input.title}
-        />
-        <br />
-        <br />
-        {questions.map((q) => {
-            return [q, answers[q.props.id].map(a => a)]
-        })}
-        <button name="question" onClick={addQuestion}>
-          Add Question
-        </button>
-        <br />
-        <br />
-        <button onClick={createQuiz}>Create Quiz</button>
-      </form>
-    </div>
-  );
+  //   <div>
+  //     <form>
+  //       <label for="title">Quiz Title</label>
+  //       <input
+  //         onChange={handleChange}
+  //         name="title"
+  //         type="text"
+  //         value={input.title}
+  //       />
+  //       <br />
+  //       <br />
+  //       {questions.map((q) => {
+  //           return [q, answers[q.props.id].map(a => a)]
+  //       })}
+  //       <button name="question" onClick={addQuestion}>
+  //         Add Question
+  //       </button>
+  //       <br />
+  //       <br />
+  //       <button onClick={createQuiz}>Create Quiz</button>
+  //     </form>
+  //   </div>
+  <div>
+    Testing again
+    <form>
+      <CreateTitle onChange={handleChange}  />
+      <CreateQuestion onChange={handleChange} />
+      <button onClick={createQuiz}>Create Quiz</button>
+    </form>
+  </div>
+   );
 }
